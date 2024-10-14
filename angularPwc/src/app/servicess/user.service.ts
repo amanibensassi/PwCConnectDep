dc216cdead4fc4ffa49e1ead70c596f007d7dd4f
Amani BEN SASSI <123634459+amanibensassi@users.noreply.github.com>
2024-10-14 14:08:30 +0200

    Update user.service.ts

--- angularPwc/src/app/servicess/user.service.ts
+++ angularPwc/src/app/servicess/user.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const Base_URL = 'http://localhost:8089/PwCGather/utilisateur';
+const Base_URL = 'http://192.168.8.142:8089/PwCGather/utilisateur';
 @Injectable({
   providedIn: 'root'
 })
