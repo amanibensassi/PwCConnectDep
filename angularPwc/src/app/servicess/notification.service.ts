ae0f2949923cc32608ba69a5788a4f3b347212f7
Amani BEN SASSI <123634459+amanibensassi@users.noreply.github.com>
2024-10-14 14:08:47 +0200

    Update notification.service.ts

--- angularPwc/src/app/servicess/notification.service.ts
+++ angularPwc/src/app/servicess/notification.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-const Base_URL = 'http://localhost:8089/PwCGather/notification';
+const Base_URL = 'http://192.168.8.142:8089/PwCGather/notification';
 @Injectable({
   providedIn: 'root'
 })
